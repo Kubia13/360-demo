a37c8cb82e99b2700f20a2f532162afee55efbdc
kubia13 <kubia@gmx.net>
2026-02-11 13:39:25 +0100

    Update App + Index

--- src/App.js
+++ src/App.js
@@ -286,12 +286,31 @@ function getScore(key) {
         )}
 
         {baseData.kfz === "Ja" && (
-          <>
-            <Question label="KFZ-Haftpflicht vorhanden?" id="kfz_haftpflicht" {...{ answers, answer }} />
-            <Question label="Schutzbrief vorhanden?" id="schutzbrief" {...{ answers, answer }} />
-            <Question label="Teilkasko oder Vollkasko vorhanden?" id="kasko" {...{ answers, answer }} />
-          </>
-        )}
+  <>
+    <Question
+      label="KFZ-Haftpflicht vorhanden?"
+      id="kfz_haftpflicht"
+      {...{ answers, answer }}
+    />
+
+    <Select
+      label="Welche KFZ-Kaskoversicherung besteht?"
+      options={["Haftpflicht", "Teilkasko", "Vollkasko", "Weiß nicht"]}
+      onChange={(v) => {
+        if (v === "Haftpflicht") answer("kasko", "haftpflicht");
+        else if (v === "Teilkasko") answer("kasko", "teilkasko");
+        else if (v === "Vollkasko") answer("kasko", "vollkasko");
+        else answer("kasko", "unbekannt");
+      }}
+    />
+
+    <Question
+      label="Schutzbrief vorhanden?"
+      id="schutzbrief"
+      {...{ answers, answer }}
+    />
+  </>
+)}
 
         <Question
   label="Rechtsschutz vorhanden?"
