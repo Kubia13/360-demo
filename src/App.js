46ae50ba1a71bb9c982643c4a38ef5a46d41ecf0
kubia13 <loefflerflorian@t-online.de>
2026-02-12 17:33:56 +0100

    Update App + Index

--- src/App.js
+++ src/App.js
@@ -200,66 +200,150 @@ const PRODUCT_STRUCTURE = {
   krankenzusatz: {
     label: "Krankenzusatzversicherung",
     products: [
-      { name: "Kombi (Mehr für Sie)", url: "#" },
-      { name: "Zahn (Mehr Zahn und Mehr Zahnvorsorge)", url: "#" },
-      { name: "Zahn (Mehr Zahn)", url: "#" },
-      { name: "Zahn (Mehr Zahnvorsorge)", url: "#" },
-      { name: "Zahn (DentPlus/ZIB)", url: "#" },
-      { name: "Ambulant (Mehr Gesundheit und Mehr Sehen)", url: "#" },
-      { name: "Ambulant (Mehr Gesundheit)", url: "#" },
-      { name: "Stationär (Mehr Komfort)", url: "#" },
-      { name: "Stationär (BKKST)", url: "#" },
-      { name: "Exclusiv+", url: "#" },
-    ]
+      {
+        name: "Kombi (Mehr für Sie)",
+        url: "https://ssl.barmenia.de/online-versichern/#/zusatzversicherung/Beitrag?adm=00840513",
+      },
+      {
+        name: "Zahn (Mehr Zahn und Mehr Zahnvorsorge)",
+        url: "https://ssl.barmenia.de/online-versichern/#/zahnversicherung/Beitrag?adm=00840513",
+      },
+      {
+        name: "Zahn (Mehr Zahn)",
+        url: "https://ssl.barmenia.de/online-versichern/#/zahnversicherung/Beitrag?tarif=1&adm=00840513",
+      },
+      {
+        name: "Zahn (Mehr Zahnvorsorge)",
+        url: "https://ssl.barmenia.de/online-versichern/#/zahnversicherung/Beitrag?tarif=2&adm=00840513",
+      },
+      {
+        name: "Zahn (DentPlus/ZIB)",
+        url: "https://ssl.barmenia.de/online-versichern/#/bkk-dentplus/Beitrag?app=bkk&adm=00840513",
+      },
+      {
+        name: "Ambulant (Mehr Gesundheit und Mehr Sehen)",
+        url: "https://ssl.barmenia.de/online-versichern/#/ambulante-zusatzversicherung/Beitrag?adm=00840513",
+      },
+      {
+        name: "Ambulant (Mehr Gesundheit)",
+        url: "https://ssl.barmenia.de/online-versichern/#/ambulante-zusatzversicherung/Beitrag?tarif=1&adm=00840513",
+      },
+      {
+        name: "Stationär (Mehr Komfort)",
+        url: "https://ssl.barmenia.de/online-versichern/#/stationaere-zusatzversicherung/Beitrag?adm=00840513",
+      },
+      {
+        name: "Stationär (BKKST)",
+        url: "https://ssl.barmenia.de/online-versichern/#/stationaere-zusatzversicherung/Beitrag?app=bkk&adm=00840513",
+      },
+      {
+        name: "Exclusiv+",
+        url: "https://ssl.barmenia.de/online-versichern/#/bkk-exclusivplus/Beitrag?app=bkk&adm=00840513",
+      },
+    ],
   },
 
   reise: {
     label: "Reiseversicherung",
     products: [
-      { name: "Travel+ / Travel Day", url: "#" },
-      { name: "BKKR", url: "#" },
-    ]
+      {
+        name: "Reiseversicherung (Travel+ und Travel day)",
+        url: "https://ssl.barmenia.de/online-versichern/#/reiseversicherung/Beitrag?adm=00840513",
+      },
+      {
+        name: "Reiseversicherung (BKKR)",
+        url: "https://ssl.barmenia.de/online-versichern/#/bkk-reise/Beitrag?adm=00840513",
+      },
+    ],
   },
 
   tier: {
     label: "Rund ums Tier",
     products: [
-      { name: "Hund, Katze und Pferd", url: "#" },
-      { name: "Hunde-KV", url: "#" },
-      { name: "Hunde-OP", url: "#" },
-      { name: "Katzen-KV", url: "#" },
-      { name: "Katzen-OP", url: "#" },
-      { name: "Pferde-OP", url: "#" },
-      { name: "Tierhalterhaftpflicht", url: "#" },
-    ]
+      {
+        name: "Hund, Katze und Pferd",
+        url: "https://ssl.barmenia.de/online-versichern/#/tierversicherung/Beitrag?adm=00840513",
+      },
+      {
+        name: "Hunde-KV",
+        url: "https://ssl.barmenia.de/online-versichern/#/tierversicherung/Beitrag?tierart=Hund&versicherung=kv&adm=00840513",
+      },
+      {
+        name: "Hunde-OP",
+        url: "https://ssl.barmenia.de/online-versichern/#/tierversicherung/Beitrag?tierart=Hund&versicherung=op&adm=00840513",
+      },
+      {
+        name: "Katzen-KV",
+        url: "https://ssl.barmenia.de/online-versichern/#/tierversicherung/Beitrag?tierart=Katze&versicherung=kv&adm=00840513",
+      },
+      {
+        name: "Katzen-OP",
+        url: "https://ssl.barmenia.de/online-versichern/#/tierversicherung/Beitrag?tierart=Katze&versicherung=op&adm=00840513",
+      },
+      {
+        name: "Pferde-OP",
+        url: "https://ssl.barmenia.de/online-versichern/#/tierversicherung/Beitrag?tierart=Pferd&versicherung=op&adm=00840513",
+      },
+      {
+        name: "Tierhalterhaftpflicht",
+        url: "https://ssl.barmenia.de/online-versichern/#/tierhalterhaftpflicht/Beitrag?adm=00840513",
+      },
+    ],
   },
 
-  haftpflicht: {
+  haus: {
     label: "Haus und Haftpflicht",
     products: [
-      { name: "Privathaftpflicht", url: "#" },
-      { name: "Hausrat", url: "#" },
-      { name: "Berufshaftpflicht (Heilberufe)", url: "#" },
-      { name: "Berufshaftpflicht (Psychologische Berufe)", url: "#" },
-    ]
+      {
+        name: "Privathaftpflicht",
+        url: "https://ssl.barmenia.de/online-versichern/#/privathaftpflicht/Beitrag?adm=00840513",
+      },
+      {
+        name: "Hausrat",
+        url: "https://ssl.barmenia.de/online-versichern/#/hausrat/Beitrag?adm=00840513",
+      },
+      {
+        name: "Berufshaftpflicht (Heilberufe)",
+        url: "https://ssl.barmenia.de/online-versichern/#/berufshaftpflicht/Beitrag?beruf=heilberufe&adm=00840513",
+      },
+      {
+        name: "Berufshaftpflicht (Psychologische Berufe)",
+        url: "https://ssl.barmenia.de/online-versichern/#/berufshaftpflicht/Beitrag?beruf=psychologen&adm=00840513",
+      },
+    ],
   },
 
   mobilitaet: {
     label: "Mobilität",
     products: [
-      { name: "Fahrradversicherung", url: "#" },
-      { name: "Kfz-Versicherung", url: "#" },
-    ]
+      {
+        name: "Fahrradversicherung",
+        url: "https://ssl.barmenia.de/online-versichern/#/fahrradversicherung/Beitrag?adm=00840513",
+      },
+      {
+        name: "Kfz-Versicherung (Kfz und Motorrad)",
+        url: "https://ssl.barmenia.de/online-versichern/#/kfzversicherung/Fahrzeug?adm=00840513",
+      },
+    ],
   },
 
   unfall: {
     label: "Unfall und Invalidität",
     products: [
-      { name: "UnfallhilfeSofort", url: "#" },
-      { name: "Unfallversicherung", url: "#" },
-      { name: "Kinder-Invaliditätsvorsorge (KISS)", url: "#" },
-    ]
-  }
+      {
+        name: "UnfallhilfeSofort",
+        url: "https://ssl.barmenia.de/online-versichern/#/unfallhilfesofort/Beitrag?adm=00840513",
+      },
+      {
+        name: "Unfallversicherung",
+        url: "https://ssl.barmenia.de/online-versichern/#/unfallversicherung/Beitrag?adm=00840513",
+      },
+      {
+        name: "Kinder-Invaliditätsvorsorge (KISS)",
+        url: "https://ssl.barmenia.de/online-versichern/#/kinder-invaliditaetsvorsorge/Beitrag?adm=00840513",
+      },
+    ],
+  },
 };
 
 export default function App() {
