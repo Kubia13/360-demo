70f9759d866dc047fa98c7316b1f60144be04648
kubia13 <loefflerflorian@t-online.de>
2026-03-01 14:43:09 +0100

    Update App + Index

--- build-version.js
+++ build-version.js
@@ -1,10 +1,15 @@
 const fs = require("fs");
 const git = require("git-rev-sync");
 
-const buildTime = new Date()
-  .toISOString()
-  .replace("T", " ")
-  .substring(0, 16);
+const now = new Date();
+
+const buildTime =
+  now.toLocaleDateString("de-DE") +
+  " " +
+  now.toLocaleTimeString("de-DE", {
+    hour: "2-digit",
+    minute: "2-digit"
+  });
 
 const gitHash = git.short();
 
@@ -13,7 +18,11 @@ REACT_APP_BUILD_TIME=${buildTime}
 REACT_APP_GIT_HASH=${gitHash}
 `;
 
+// Production
 fs.writeFileSync(".env.production.local", content.trim());
+
+// Development
+fs.writeFileSync(".env.development.local", content.trim());
 
 console.log("Build version generated:");
 console.log(content);
